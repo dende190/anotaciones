2e55e635ddb995e481fe25368102789e57be164a
Juan Pablo Arnedo <juan.arnedo@zalvadora.com>
2022-01-22 12:44:51 -0500

    prueba de carga de archivos publcios del servidor de api

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 const {config} = require('./config/config');
 const usersRoute = require('./routes/users');
 const notesRoute = require('./routes/notes');
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'assets')));
 
 //Routes
 usersRoute(app);
